a19f05c323210442392e27a33a38ef3cc03d4c24
Sofia <sofya.vedenina.2005@gmail.com>
2025-03-01 11:05:22 +0300

    Added application window class

---
+++ Window.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+class Window:
+    """Class responsible for the application window"""
+    window = Tk()
+    window.title("Chameleon")
+    window.geometry("1920x1080")
+    window.mainloop()
