a1649e045d16379dce86fd9c35f74c24178f6a36
Sofia <sofya.vedenina.2005@gmail.com>
2025-03-05 11:47:09 +0300

    Added description of methods in Window.py

--- Window.py
+++ Window.py
@@ -3,6 +3,7 @@ from tkinter import *
 class Window(Tk):
     """Class responsible for the application window"""
     def __init__(self, size="1920x1080"):
+        """Initialization"""
         super().__init__()
         self.title("Chameleon")
         self.geometry(size)
@@ -11,5 +12,6 @@ class Window(Tk):
         self.maxsize(1920, 1080)
         self.protocol("WM_DELETE_WINDOW", self.finish)
     def finish(self):
+        """Closing"""
         self.destroy()
         print("Closing Chameleon")
