6099b8907c4ff93851aca6c7ce3d1801f7be3851
Sofia <sofya.vedenina.2005@gmail.com>
2025-03-05 11:41:31 +0300

    Added Window.py and main.py

---
+++ Window.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+class Window(Tk):
+    """Class responsible for the application window"""
+    def __init__(self, size="1920x1080"):
+        super().__init__()
+        self.title("Chameleon")
+        self.geometry(size)
+        self.resizable(True, True)
+        self.minsize(200, 150)
+        self.maxsize(1920, 1080)
+        self.protocol("WM_DELETE_WINDOW", self.finish)
+    def finish(self):
+        self.destroy()
+        print("Closing Chameleon")
---
+++ main.py
@@ -0,0 +1,6 @@
+from tkinter import *
+from Window import *
+
+if __name__ == '__main__':
+    root = Window()
+    root.mainloop()
