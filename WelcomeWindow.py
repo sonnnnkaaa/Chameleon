6dcec18bcd0361780b369c9f8bb921401fb0f222
Sofia <sofya.vedenina.2005@gmail.com>
2025-03-07 14:37:18 +0300

    Added directory_selecting.py

--- WelcomeWindow.py
+++ WelcomeWindow.py
@@ -1,5 +1,6 @@
 from Window import *
 from file_selecting import *
+from directory_selecting import *
 
 class WelcomeWindow(Window):
     def __init__(self):
@@ -8,8 +9,8 @@ class WelcomeWindow(Window):
         y = self.winfo_screenheight()
         self.geometry('{}x{}+{}+{}'.format(int(x * 0.7), int(y * 0.7), 0, 0))
         create_project_btn = Button(text="Create project")
-        open_project_btn = Button(text="Open existing project")
-        upload_btn = Button(text="Upload an image", command=select_file)
+        open_project_btn = Button(text="Open existing project", command=select_directory, textvariable=dir_name)
+        upload_btn = Button(text="Upload an image", command=select_file, textvariable=filename)
         create_project_btn.pack()
         open_project_btn.pack()
         upload_btn.pack()
--- file_selecting.py
+++ file_selecting.py
@@ -1,16 +1,18 @@
 from tkinter.filedialog import *
 from datetime import datetime as dt
-import os
+
+filename = ""
 
 def select_file():
     _filetypes = [('JPEG pictures', '*.jpg'),
                   ('PNG pictures', '*.png'),
                   ('All files', '*.*')]
     print(dt.now().time(), "File selecting window is opened")
+    global filename
     filename = askopenfilename(title="Select a file", initialdir="/", filetypes=_filetypes)
     if filename == () or filename == "":
         print(dt.now().time(), "Nothing is selected...")
     else:
-        with open(filename, "rb") as selected_file: #rb - двоичный режим (без него не работает)
-            text = selected_file.read()
+        # with open(filename, "rb") as selected_file: #rb - двоичный режим (без него не работает)
+        #     content = selected_file.read()
         print(dt.now().time(), filename, "is selected")
