bc5a3e7eea9b67b5d7bb3fca042b512f6679c6f2
sonnnnkaaa <sofya.vedenina2005@gmail.com>
2025-03-14 13:29:33 +0300

    Added click_create_btn function in CreateProjectWindow.py

--- CreateProjectWindow.py
+++ CreateProjectWindow.py
@@ -1,17 +1,27 @@
 from Window import *
 from directory_selecting import *
-
-project_name = ""
+from directory_creating import *
+import os
 
 class CreateProjectWindow(Window):
     def __init__(self):
         super().__init__("Create new project")
+        self.project_name = None
         x = self.winfo_screenwidth()
         y = self.winfo_screenheight()
         self.geometry('{}x{}+{}+{}'.format(int(x * 0.3), int(y * 0.5), 0, 0))
-        choose_dir_btn = Button(text="Choose directory", command=select_directory, textvariable=dir_name)
-        project_name_input = Entry()
-        project_name_input.pack()
-        choose_dir_btn.pack()
-    def choosing_dir(self):
-        
+        self.project_name_input = Entry(master=self)
+        self.project_name_input.pack()
+        self.create_proj_btn = Button(master=self,
+                                      text="Create",
+                                      command=self.click_create_btn)
+        self.create_proj_btn.pack()
+    def click_create_btn(self):
+        self.create_proj_btn["state"] = "disabled"
+        self.project_name = self.project_name_input.get()
+        if create_directory(self.project_name):
+
+        else:
+            self.create_proj_btn["state"] = "normal"
+
+
