232c83be5add78d1c17c01baf4530e3f20567c92
sonnnnkaaa <sofya.vedenina2005@gmail.com>
2025-03-14 13:31:41 +0300

    directory_creating.py has extended (added error handling, the function itself now works on a different principle)

--- directory_creating.py
+++ directory_creating.py
@@ -2,15 +2,29 @@ from tkinter.filedialog import *
 from tkinter import *
 from tkinter.ttk import *
 from datetime import datetime as dt
+from directory_selecting import *
 import os
+from Message import *
 
-new_dir_name = ""
+def create_directory(new_dir_name):
+    if new_dir_name == "":
+        print(dt.now().time(), "Empty field is entered")
+        open_warning("You have not entered the project name, please enter the name of your project")
+        return
+    print(dt.now().time(), "Directory creating window is opened")
+    parent_dir = select_directory()
+    if not os.access(parent_dir, os.W_OK):
+        print(dt.now().time(), f"No write permissions for", parent_dir)
+    try:
+        os.mkdir(parent_dir + "/" + new_dir_name)
+    except OSError:
+        print(dt.now().time(), "New directory was not created because it already exists")
+        open_warning("A directory " + new_dir_name + " already exists")
+        return
+    # except TypeError:
+    #     open_warning("Nothing is selected")
+    #     return
+    created_directory_path = parent_dir + "/" + new_dir_name
+    print(dt.now().time(), created_directory_path, "is created")
+    return created_directory_path
 
-def create_directory():
-    print(dt.now().time(), "File selecting window is opened")
-    global new_dir_name
-    parent_dir = askdirectory(title="Select a directory in which to save the new project", initialdir="/", mustexist=False)
-
-    while True:
-
-        try:
