e36d459870af30ab02a173247d3a986b6bcfd3a7
sonnnnkaaa <sofya.vedenina2005@gmail.com>
2025-03-14 13:32:34 +0300

    select_directory now returns selected directory name

--- directory_selecting.py
+++ directory_selecting.py
@@ -1,13 +1,11 @@
 from tkinter.filedialog import *
 from datetime import datetime as dt
 
-dir_name = ""
-
 def select_directory():
     print(dt.now().time(), "Directory selecting window is opened")
-    global dir_name
     dir_name = askdirectory(title="Select a directory", initialdir="/", mustexist=False)
     if dir_name == () or dir_name == "":
         print(dt.now().time(), "Nothing is selected...")
     else:
         print(dt.now().time(), dir_name, "is selected")
+        return dir_name
