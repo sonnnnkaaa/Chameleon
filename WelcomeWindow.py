2c69f9ad1205bc12e0130873b72ae4b23d41f6f5
sonnnnkaaa <sofya.vedenina2005@gmail.com>
2025-03-14 13:35:31 +0300

    Added functions that are commands for buttons

--- WelcomeWindow.py
+++ WelcomeWindow.py
@@ -1,6 +1,7 @@
 from Window import *
 from file_selecting import *
 from directory_selecting import *
+from CreateProjectWindow import *
 
 class WelcomeWindow(Window):
     def __init__(self):
@@ -8,10 +9,21 @@ class WelcomeWindow(Window):
         x = self.winfo_screenwidth()
         y = self.winfo_screenheight()
         self.geometry('{}x{}+{}+{}'.format(int(x * 0.7), int(y * 0.7), 0, 0))
-        create_project_btn = Button(text="Create project")
-        open_project_btn = Button(text="Open existing project", command=select_directory, textvariable=dir_name)
-        upload_btn = Button(text="Upload an image", command=select_file, textvariable=filename)
+        create_project_btn = Button(text="Create empty project", command=self.create_chosen) # открытие нового окна
+        open_project_btn = Button(text="Open existing project", command=self.open_chosen)
+        upload_btn = Button(text="Upload an image", command=self.upload_chosen)
         create_project_btn.pack()
         open_project_btn.pack()
         upload_btn.pack()
         print(dt.now().time(), "Welcome window is opened")
+    def create_chosen(self):
+        print(dt.now().time(), "\"Create empty project\" option is chosen")
+        create_project_win = CreateProjectWindow()
+    def open_chosen(self):
+        print(dt.now().time(), "\"Open existing project\" option is chosen")
+        chosen_dir = select_directory()
+        #продолжение в проекте уже
+    def upload_chosen(self):
+        print(dt.now().time(), "\"Upload an image\" option is chosen")
+        chosen_image = select_file()
+        #продолжение
