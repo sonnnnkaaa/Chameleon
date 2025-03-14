e2e156134662efa8f5eff0929723d8dbf47bb92e
sonnnnkaaa <sofya.vedenina2005@gmail.com>
2025-03-14 13:33:05 +0300

    select_file now returns selected file name

--- file_selecting.py
+++ file_selecting.py
@@ -1,14 +1,12 @@
 from tkinter.filedialog import *
 from datetime import datetime as dt
 
-filename = ""
 
 def select_file():
     _filetypes = [('JPEG pictures', '*.jpg'),
                   ('PNG pictures', '*.png'),
                   ('All files', '*.*')]
     print(dt.now().time(), "File selecting window is opened")
-    global filename
     filename = askopenfilename(title="Select a file", initialdir="/", filetypes=_filetypes)
     if filename == () or filename == "":
         print(dt.now().time(), "Nothing is selected...")
@@ -16,3 +14,4 @@ def select_file():
         # with open(filename, "rb") as selected_file: #rb - двоичный режим (без него не работает)
         #     content = selected_file.read()
         print(dt.now().time(), filename, "is selected")
+        return filename
