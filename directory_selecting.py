fd271e80d4c153374b959f6d25f6446f044919e8
sonnnnkaaa <sofya.vedenina2005@gmail.com>
2025-03-27 13:34:45 +0300

    Added check for empty choice

--- directory_selecting.py
+++ directory_selecting.py
@@ -6,6 +6,8 @@ def select_directory():
     dir_name = askdirectory(title="Select a directory", initialdir="/", mustexist=False)
     if dir_name == () or dir_name == "":
         print(dt.now().time(), "Nothing is selected...")
+        return
     else:
         print(dt.now().time(), dir_name, "is selected")
         return dir_name
+
