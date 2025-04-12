81945d5ccad37a61873c8d1b3f4427794d25c373
sonnnnkaaa <sofya.vedenina2005@gmail.com>
2025-04-12 15:08:16 +0300

    Create project button update

--- CreateProjectWindow.py
+++ CreateProjectWindow.py
@@ -10,6 +10,10 @@ class CreateProjectWindow(Window):
         x = self.winfo_screenwidth()
         y = self.winfo_screenheight()
         self.geometry('{}x{}+{}+{}'.format(int(x * 0.3), int(y * 0.5), 0, 0))
+        self.go_back_btn = Button(master=self,
+                                  text="Go back",
+                                  command=self.finish)
+        self.go_back_btn.pack()
         self.project_name_input = Entry(master=self)
         self.project_name_input.pack()
         self.create_proj_btn = Button(master=self,
@@ -18,10 +22,14 @@ class CreateProjectWindow(Window):
         self.create_proj_btn.pack()
     def click_create_btn(self):
         self.create_proj_btn["state"] = "disabled"
+        self.go_back_btn["state"] = "disabled"
         self.project_name = self.project_name_input.get()
         if create_directory(self.project_name):
-
+            self.finish()
+            WorkAreaWindow(self.project_name)
         else:
             self.create_proj_btn["state"] = "normal"
-
+            self.go_back_btn["state"] = "normal"
+    def click_goback_btn(self):
+        self.finish()
 
